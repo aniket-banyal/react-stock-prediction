58e3edb2e8844bbeb314cb900026c36506c56ae2
aniket <dummyani26@gmail.com>
2021-08-25 09:29:16 +0530

    Use react-select

--- src/components/models.js
+++ src/components/models.js
@@ -1,18 +1,20 @@
 import ModelList from './modelList'
-import Search from './search'
 import { useState } from 'react'
+import Select from 'react-select'
 
 function Models({ models, tickers }) {
-    console.log('models render')
+    console.log('Models render')
+
     const [searchValue, setSearchValue] = useState('')
+    const options = tickers.map(ticker => ({ value: ticker.symbol, label: ticker.symbol }))
 
     return (
         <div className='models'>
-            <Search options={tickers} value={searchValue} setSearchValue={setSearchValue} />
+            <Select options={options} placeholder='Select ticker' autoFocus isClearable
+                onChange={option => setSearchValue(option?.value)} />
             <ModelList models={models} searchValue={searchValue} />
         </div>
     )
-
 }
 
 export default Models
--- src/components/predictions.js
+++ src/components/predictions.js
@@ -1,16 +1,20 @@
 import PredictionList from './predictionList'
-import Search from './search'
 import { useState } from 'react'
+import Select from 'react-select'
 
 
 function Predictions({ models, tickers }) {
     console.log('Predictions render')
+
     const [searchValue, setSearchValue] = useState('')
     const [predictionDate, setPredictionDate] = useState('')
+    const options = tickers.map(ticker => ({ value: ticker.symbol, label: ticker.symbol }))
 
     return (
         <div className='predictions'>
-            <Search options={tickers} value={searchValue} setSearchValue={setSearchValue} />
+
+            <Select options={options} placeholder='Select ticker' autoFocus isClearable
+                onChange={option => setSearchValue(option?.value)} />
             <p> Predictions for: {predictionDate} </p>
             <PredictionList models={models} searchValue={searchValue} setPredictionDate={setPredictionDate} />
         </div>

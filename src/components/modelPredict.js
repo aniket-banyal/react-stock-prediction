3aab169aaaecf10a0ff35b204f6ce904586c2d15
aniket <dummyani26@gmail.com>
2021-09-02 15:42:05 +0530

    Use datepicker from mui

--- src/components/modelPredict.js
+++ src/components/modelPredict.js
@@ -1,10 +1,13 @@
-import { Container } from "@material-ui/core"
+import { Button, Container, TextField, Typography } from "@material-ui/core"
+import { DatePicker, LocalizationProvider } from "@material-ui/lab"
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns'
+import { format } from "date-fns"
 import { useState } from "react"
 
 function ModelPredict({ model }) {
     console.log('ModelPredict render')
 
-    const [predictionDate, setPredictionDate] = useState('')
+    const [predictionDate, setPredictionDate] = useState(null)
     const [predictionValue, setPredictionValue] = useState('')
     const [isLoading, setIsLoading] = useState(false)
 
@@ -15,22 +18,63 @@ function ModelPredict({ model }) {
             pred_date: predictionDate,
         }))
         const predictionValue = await res.json()
-        console.log(predictionValue)
         setPredictionValue(predictionValue)
         setIsLoading(false)
     }
 
+    const handlePredictionDateChange = date => {
+        const formattedDate = format(date, 'yyyy-MM-dd')
+        setPredictionDate(formattedDate)
+    }
+
     return (
-        <Container>
+        <Container
+            sx={{
+                m: 5,
+                width: '100%',
+                height: '100%'
+            }}
+        >
             {model ?
-                <div>
-                    <form onSubmit={fetchPrediction} >
-                        <label>Select Prediction date: </label>
-                        <input type="date" name="date" value={predictionDate} onInput={e => setPredictionDate(e.target.value)} required />
-                        <input type="submit" value="Submit" />
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '50%',
+                        height: '30%',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <form
+                        onSubmit={fetchPrediction}
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: '50%',
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DatePicker
+                                label="Prediction Date"
+                                value={predictionDate}
+                                onChange={handlePredictionDateChange}
+                                inputFormat='dd-MM-yyyy'
+                                mask='__-__-____'
+                                renderInput={(params) => <TextField {...params} required />}
+                            />
+                        </LocalizationProvider>
+                        <Button
+                            type='submit'
+                            variant='contained'
+                        >
+                            Predict
+                        </Button>
                     </form>
-                    {isLoading && <p>Predicting ...</p>}
-                    {!isLoading && predictionValue && <p>Prediction: {predictionValue.prediction}</p>}
+                    <Typography variant='h5'>
+                        {isLoading && 'Predicting ...'}
+                        {!isLoading && predictionValue && `Prediction: ${predictionValue.prediction}`}
+                    </Typography>
                 </div >
                 : <div>Loading....</div>
             }

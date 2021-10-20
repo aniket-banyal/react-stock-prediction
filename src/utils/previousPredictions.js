7732afa68d50d009528d75a99dedbcf5e3e44512
aniket <dummyani26@gmail.com>
2021-10-20 21:33:48 +0530

    Change API url

--- src/utils/previousPredictions.js
+++ src/utils/previousPredictions.js
@@ -4,7 +4,7 @@ import { getMarketClosed } from '../utils/latestPrediction'
 const fetchPredictions = async (ticker, days) => {
     console.log('---fetchPredictions called---')
 
-    const res = await fetch(`https://react-stock-prediction.herokuapp.com/api/predictions/${ticker}/?` + new URLSearchParams({
+    const res = await fetch(`https://django-stock-prediction.herokuapp.com/api/predictions/${ticker}/?` + new URLSearchParams({
         period: days,
     }))
     const predictions = await res.json()
